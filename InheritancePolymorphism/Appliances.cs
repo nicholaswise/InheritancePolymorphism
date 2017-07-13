08498515a8a1a595e87ee3fc056fdeacaaa6c99c
Nick Wise <nickcwise@gmail.com>
2017-07-13 15:51:56 -0400

    updated exercises

---
+++ InheritancePolymorphism/Appliances.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphism
+{
+    abstract class Appliances
+    {
+        public abstract void Clean();
+    }
+}
